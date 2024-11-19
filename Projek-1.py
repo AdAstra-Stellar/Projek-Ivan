9c4a23e4c692ba90af3fa7e4fd3a9cc0e8d17a8d
AdAstra-Stellar <muhammadivann107@gmail.com>
2024-11-19 08:19:07 +0700

    Update Projek-1.py
    Penambahan self aware

--- Projek-1.py
+++ Projek-1.py
@@ -1,2 +1,4 @@
 Projek Ivan
 Projek ini berisikan mengenai proses dan progres cara pembuatan AI MU/TH/UR
+Projek ini bervisi untuk membuat AI yang dapat digunakan untuk eksplorasi angkasa
+Self aware
